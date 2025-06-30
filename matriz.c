ca4ea927a04240763f95ec96321eb7ed57c8f086
guilhermedreh <151393740+guilhermedreh@users.noreply.github.com>
2025-06-30 20:13:21 -0300

    Add files via upload

---
+++ matriz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int mat[2][2];
+    int i, j;
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("Digite um valor para [%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);  // Lê o valor e armazena na matriz
+        }
+    }
+
+    // Opcional: mostrar os valores lidos
+    printf("Matriz digitada:\n");
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
